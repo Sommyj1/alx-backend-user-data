f98d113b1a1a069800201ba34b5a4a834b0fedd7
Sommyj1 <113688978+Sommyj1@users.noreply.github.com>
2023-09-07 19:14:37 -0700

    auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/auth(1).py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" Module of class Auth to manage the API authentication.
+"""
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+from os import environ
+
+
+class Auth:
+    """ Class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to require a auth.
+        """
+        if path is None:
+            return True
+        if (excluded_paths is None) or (excluded_paths == []):
+            return True
+
+        path = path + "/" if path[-1] != "/" else path
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif excluded_path == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to get the authorization header.
+        """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get the current user.
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request
+        """
+        if request is None:
+            return None
+
+        cookie_name = environ.get("SESSION_NAME", "")
+        return request.cookies.get(cookie_name)
